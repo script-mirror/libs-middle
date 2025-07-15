f2fa23dd8c9fa9acd38de331ab6f31cd396e7ef1
raizen-arthurmoraes <arthur.moraes2@raizen.com>
2025-07-15 15:10:01 -0300

    chore: update version

--- middle/__init__.py
+++ middle/__init__.py
@@ -1,8 +1,4 @@
 """Middle - Raizen Power Trading Libraries Middle Layer Utilities"""
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __author__ = "Raizen Power Middle"
-
-import middle.utils as utils
-
-__all__ = ["utils"]
