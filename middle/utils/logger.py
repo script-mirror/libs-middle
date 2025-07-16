e25273e35ab54aa31765f553f4ccaa2d4bc53b05
raizen-arthurmoraes <arthur.moraes2@raizen.com>
2025-07-16 16:11:45 -0300

    refact: movendo logger para utils e renomeando instancia do logger no decoomp para evitar conflito/sobrescrita

--- middle/utils/logger.py
+++ middle/utils/logger.py
@@ -5,7 +5,7 @@ import os
 def setup_logger(log_path: str = None):
     logger = logging.getLogger(log_path if log_path else "default_logger")
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(levelname)s:\t%(asctime)s\t %(name)s.py:%(lineno)d\t %(message)s')
     # Remove handlers duplicados
     if logger.hasHandlers():
         logger.handlers.clear()
