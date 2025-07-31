1c7db6c018f61e476a5e73706ad6a443a20d5207
raizen-arthurmoraes <arthur.moraes2@raizen.com>
2025-07-31 16:31:09 -0300

    feat: funcao para retornar ultimos produtos disponiveis no webhook

--- middle/s3.py
+++ middle/s3.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import datetime
 from .utils import (
     get_auth_header,
     constants,
@@ -21,3 +22,32 @@ def handle_webhook_file(webhook_payload: dict, path_download: str) -> str:
     with open(filename, 'wb') as f:
         f.write(file_content.content)
     return filename
+
+
+def get_latest_webhook_product(
+    nome_produto: str,
+    data: datetime.date = datetime.date.today(),
+    date_range:int = 35,
+) -> dict:
+    
+    """Busca um produto especifico no webhook, disponivel nos ultimos x dias.
+
+    Args:
+        nome_produto (str): nome do produto a ser buscado(mesmo nome disponibilizado pela sintegre).
+        data (datetime.date, optional): data maxima de publicacao do produto Defaults to today.
+        date_range (int, optional): Quantidade de dias para tras a buscar o produto Defaults to 35.
+
+    Returns:
+        dict: dicionario com todos os produtos encontrados no periodo selecionado.
+    """
+    res = requests.get(
+        f"{constants.BASE_URL}/webhook/api/webhooks/timeline/filtered",
+        {"nome": nome_produto,
+         "startDate": data - datetime.timedelta(days=date_range)},
+        headers=get_auth_header()
+    )
+    if res.status_code == 200:
+        return res.json()['groups']['events']
+    else:
+        raise Exception(
+            f"Erro ao buscar dados: {res.status_code} - {res.text}")
