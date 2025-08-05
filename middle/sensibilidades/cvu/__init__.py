bf2b6968aa17f5cd5ba120b5acf0e9f71f9a3df2
raizen-arthurmoraes <arthur.moraes2@raizen.com>
2025-08-05 17:04:36 -0300

    feat: update decomp cvu

--- middle/sensibilidades/cvu/__init__.py
+++ middle/sensibilidades/cvu/__init__.py
@@ -3,6 +3,10 @@ from ._gerar_cvu_newave import (
     atualizar_cvu_clast_conjuntural,
 )
 
+from ._gerar_cvu_decomp import (
+    atualizar_cvu_dadger_decomp,
+)
+
 from .gerar_cvu import (
     get_cvu_trusted,
 )
@@ -10,5 +14,6 @@ from .gerar_cvu import (
 __all__ = [
     "atualizar_cvu_clast_estrutural",
     "atualizar_cvu_clast_conjuntural",
+    "atualizar_cvu_dadger_decomp",
     "get_cvu_trusted",
 ]
