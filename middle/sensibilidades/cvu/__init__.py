06e53d9cee739f8dcc8bda57dbf8bf3e6a28d745
raizen-arthurmoraes <arthur.moraes2@raizen.com>
2025-08-05 15:18:54 -0300

    fix: import get_cvu

--- middle/sensibilidades/cvu/__init__.py
+++ middle/sensibilidades/cvu/__init__.py
@@ -3,6 +3,10 @@ from ._gerar_cvu_newave import (
     atualizar_cvu_clast_conjuntural,
 )
 
+from .gerar_cvu import (
+    get_cvu_trusted,
+)
+
 __all__ = [
     "atualizar_cvu_clast_estrutural",
     "atualizar_cvu_clast_conjuntural",
