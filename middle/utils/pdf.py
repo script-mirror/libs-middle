81b7ac7cd93440a00b04c45d60131659d92c232e
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-09-01 15:30:08 -0300

    refact: retornando bytes e salvar opcional

--- middle/utils/pdf.py
+++ middle/utils/pdf.py
@@ -1,15 +1,22 @@
 from pdf2image import convert_from_path
+from io import BytesIO
 
 def pdf_to_jpg(
-    path:str,
-    page_number:int,
-    path_output:str
+    path: str,
+    page_number: int,
+    path_output: str = None
     ):
     """ Converte PDF para JPG
     :param path: [str] Path do arquivo PDF a ser convertido
-    :param path_output: [str] Path do arquivo JPG final
+    :param path_output: [str|None] Path do arquivo JPG final (opcional)
     :param page_number: [int] Numero da pagina a ser convertida
-    :return None: 
+    :return bytes: Bytes da imagem JPG
     """
     pages = convert_from_path(path, 100)
-    pages[page_number-1].save(path_output, 'JPEG')
+    img = pages[page_number-1]
+    img_bytes = BytesIO()
+    img.save(img_bytes, 'JPEG')
+    img_bytes.seek(0)
+    if path_output is not None:
+        img.save(path_output, 'JPEG')
+    return img_bytes.getvalue()
