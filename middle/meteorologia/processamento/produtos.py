d5107541d12d8655450fb3d7fa96e285fc33b039
josepaulo1233 <josehpaulo2124@gmail.com>
2025-09-05 14:53:08 -0300

    refactor: comment out unused CSV saving logic and related data processing in GeraProdutosPrevisao class

--- middle/meteorologia/processamento/produtos.py
+++ middle/meteorologia/processamento/produtos.py
@@ -3061,24 +3061,24 @@ class GeraProdutosPrevisao:
                         
                     })
 
-                    path_to_save = f'/WX2TB/Documentos/saidas-modelos/NOVAS_FIGURAS/csv_eolica/{self.modelo_fmt}'
-                    df.to_csv(f'{path_to_save}/{self.modelo_fmt}_{area}_{self.data_fmt}_diario.csv')
+                    # path_to_save_csv = f'/WX2TB/Documentos/saidas-modelos/NOVAS_FIGURAS/csv_eolica/{self.modelo_fmt}'
+                    # df.to_csv(f'{path_to_save_csv}/{self.modelo_fmt}_{area}_{self.data_fmt}_diario.csv')
 
                     # Titulo do plot
                     titulo = f'{self.modelo_fmt.upper()} - Magnitude do vento a 100m - {area.replace("_", " ")}\nCondição Inicial: {self.cond_ini} \u2022 Climatologia ERA5 [1991-2020]'
                     filename = f'{path_to_save}/mag_vento100_{area}'
                     plot_graficos_2d(df=df, tipo='vento', titulo=titulo, filename=filename)
 
-                    if index > 0:
-                        df_temp = pd.concat([df, df_temp], axis=1)
+                    # if index > 0:
+                    #     df_temp = pd.concat([df, df_temp], axis=1)
 
-                    else:
-                        df_temp = df
+                    # else:
+                    #     df_temp = df
 
-                colunas = [x for x in df_temp.columns if 'Climatologia' not in x]
-                df_media = pd.DataFrame(df_temp[colunas].mean(axis=1), columns=list(set(colunas)))
-                df_media['Climatologia'] = df_temp['Climatologia'].mean(axis=1)
-                df_media.to_csv(f'{path_to_save}/{self.modelo_fmt}_MEDIANORDESTE_{self.data_fmt}_diario.csv')
+                # colunas = [x for x in df_temp.columns if 'Climatologia' not in x]
+                # df_media = pd.DataFrame(df_temp[colunas].mean(axis=1), columns=list(set(colunas)))
+                # df_media['Climatologia'] = df_temp['Climatologia'].mean(axis=1)
+                # df_media.to_csv(f'{path_to_save_csv}/{self.modelo_fmt}_MEDIANORDESTE_{self.data_fmt}_diario.csv')
 
             elif modo == 'pnmm_vento850':
 
