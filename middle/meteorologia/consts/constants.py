d987937c40459479559614c700239950591da279
josepaulo1233 <josehpaulo2124@gmail.com>
2025-09-16 13:11:44 -0300

    Add 'chuva_boletim_consumidores' to labels_variaveis in constants

--- middle/meteorologia/consts/constants.py
+++ middle/meteorologia/consts/constants.py
@@ -16,7 +16,7 @@ CONSTANTES = {
 
     'labels_variaveis': {
 
-        '[mm]': ['tp', 'chuva_ons', 'prec', 'acumulado_total', 'wind_prec_geop', 'diferenca', 'desvpad', 'tp_anomalia', 'chuva_acumualada_merge', 'chuva_acumualada_merge_anomalia'],
+        '[mm]': ['tp', 'chuva_ons', 'prec', 'acumulado_total', 'wind_prec_geop', 'diferenca', 'desvpad', 'tp_anomalia', 'chuva_acumualada_merge', 'chuva_acumualada_merge_anomalia', 'chuva_boletim_consumidores'],
         '[m/s]': ['wind200', 'mag_vento100', 'mag_vento100_anomalia'],
         '[dam]': ['geop_500'],
         '[1/s]': ['vorticidade', 'divergencia', 'divergencia850'],
