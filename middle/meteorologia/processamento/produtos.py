8cb15c32bf2cfc731b7b7a957b390bdb01a035f6
josepaulo1233 <josehpaulo2124@gmail.com>
2025-09-22 16:50:33 -0300

    feat: update modelo_fmt to handle '-ons' suffix and add notifications for operational weeks panel

--- middle/meteorologia/processamento/produtos.py
+++ middle/meteorologia/processamento/produtos.py
@@ -834,8 +834,7 @@ class GeraProdutosPrevisao:
     def __init__(self, produto_config_sf, tp_params=None, pl_params=None, shapefiles=None, produto_config_pl=None, modo_atual=True):
 
         self.produto_config_sf = produto_config_sf
-        # self.modelo_fmt = self.produto_config_sf.modelo if 'membros' not in self.produto_config_sf.modelo else self.produto_config_sf.modelo.replace('-membros', '')
-        self.modelo_fmt = self.produto_config_sf.modelo.replace('-membros', '').replace('-wind', '')
+        self.modelo_fmt = self.produto_config_sf.modelo.replace('-membros', '').replace('-wind', '').replace('-ons', '')
         self.arquivos_com_membros = True if '-membros' in self.produto_config_sf.modelo else False
         self.arquivos_com_wind = True if '-wind' in self.produto_config_sf.modelo else False
         self.output_path = self.produto_config_sf.output_path
@@ -3810,6 +3809,11 @@ class GeraProdutosPrevisao:
                                     shapefiles=self.shapefiles, 
                                     variavel_plotagem=variavel_plotagem,
                                     column_plot='vl_chuva', _type='bruto', filename=formato_filename(self.modelo_fmt, f'semana_energ-r{self.data_fmt}', semana)) # PCONJUNTO_3semana_energ-r2025092200.png
+
+                path_painel = painel_png(path_figs=path_to_save, output_file=f'painel_semanas_operativas_{self.modelo_fmt}_{self.data_fmt}.png')
+                send_whatsapp_message(destinatario=Constants().WHATSAPP_METEOROLOGIA, mensagem=f'{self.modelo_fmt.upper()} {self.cond_ini}', arquivo=path_painel)
+                send_email_message(mensagem=f'MAPAS {self.modelo_fmt.upper()} {self.cond_ini}', arquivos=[path_painel], assunto=f'MAPAS {self.modelo_fmt.upper()} {self.cond_ini}', destinatario=[Constants().EMAIL_MIDDLE, Constants().EMAIL_FRONT])
+                print(f'Removendo painel ... {path_painel}')
 
             if acumulado_total:
 
