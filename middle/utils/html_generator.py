b8c044571db82495f2910b7cc8474cf5356e956d
GilseuRaizen <gilseu.muhlen@raizen.com>
2025-09-27 18:40:20 -0300

    feat: enhance HtmlBuilder to generate complete HTML tables with customizable widths and alternating row colors

--- middle/utils/html_generator.py
+++ middle/utils/html_generator.py
@@ -62,3 +62,73 @@ def gera_tabela_html(
     html += '</tbody></table>'
     
     return f'{style} {html}'
+
+
+class HtmlBuilder:
+    def __init__(self):
+        self.table_generators = {
+            'resultados_prospec': self._gerar_tabela_resultados_prospec,
+        }
+
+    def _gerar_tabela_resultados_prospec(self, dados) -> str:
+        """
+        Gera um documento HTML completo contendo todas as tabelas de resultados de prospecção
+
+        Args:
+            dados: [dict] Um dicionário contendo os dados necessários para gerar as tabelas. Deve conter as chaves:
+                - 'body': Lista de listas com os dados das células do corpo da tabela
+                - 'header': Lista com os cabeçalhos das colunas
+                - 'width_colunas': Lista com as larguras das colunas (em pontos)
+                - 'nth_color': Número de linhas para alternar a cor 
+
+        Returns:
+            str: HTML completo com todas as tabelas
+        """
+        
+        body = dados.get('body', [])
+        header = dados.get('header', [])
+        width_colunas = dados.get('width_colunas', [])
+        nth_color = 1
+
+        tamanho_default_colunas = 61 #pt
+        if width_colunas == [] and header != []:
+            for i in range(len(header)+1):
+                width_colunas.append(tamanho_default_colunas)
+
+        tamanho_tabela = 0
+        for tam in width_colunas:
+            tamanho_tabela += tam
+
+        html = '<table style="width:{}pt;border-collapse:collapse" width="{}" cellspacing="0" cellpadding="0" border="0"><tbody>\n'.format(int(tamanho_tabela*1.12), int(tamanho_tabela*1.12*1.33))
+        if header != []:
+            html += '\t<tr style="height: 15pt;">\n'
+            for i, cell in enumerate(header):
+                html += '\t\t<td style="width: {}pt; border: 1pt solid windowtext; background: rgb(100,100,100) none repeat scroll 0% 0%; padding: 0cm 3.5pt; height: 15pt;" width="{}" valign="bottom" nowrap="nowrap">\n'.format(width_colunas[i], int(width_colunas[i]*1.33))
+                html += '\t\t<p class="MsoNormal" style="margin: 0cm 0cm 0.0001pt; text-align: center; line-height: normal; font-size: 11pt; font-family: &quot;Calibri&quot;, sans-serif;" align="center"><span style="color: white;">{}<span></span></span></p>\n'.format(cell)
+                html += '\t\t</td>\n'
+            html += '\t\t</tr>\n'
+
+        cor_verde = False
+        if body != []:
+            for index_linha, linha in enumerate(body):
+
+                # a cada n iteracoes, a cor e alternada entre branco e verde
+                if index_linha % nth_color == 0:
+                    cor_verde = not cor_verde
+
+                html += '\t<tr style="height: 15pt;">\n'
+                for cell in linha:
+                    # if index_linha%2 == 0:
+                    if cor_verde:
+                        html += '\t\t<td style="width:49pt;border-color:currentcolor windowtext windowtext;border-style:none solid solid;border-width:medium 1pt 1pt;background:white none repeat scroll 0% 0%;padding:0cm 3.5pt;height:15pt" width="65" valign="bottom" nowrap="">\n'
+                        html += '\t\t<p class="MsoNormal" style="margin:0cm 0cm 0.0001pt;text-align:center;line-height:normal;font-size:11pt;font-family:&quot;Calibri&quot;,sans-serif" align="center"><span style="color:black"><span>&nbsp;</span>{}<span></span></span></p>\n'.format(cell)
+                        html += '\t\t</td>\n'
+
+                    else:
+                        html += '\t\t<td style="width:49pt;border-color:currentcolor windowtext windowtext;border-style:none solid solid;border-width:medium 1pt 1pt;background:rgb(200,200,200) none repeat scroll 0% 0%;padding:0cm 3.5pt;height:15pt" width="65" valign="bottom" nowrap="">\n'
+                        html += '\t\t<p class="MsoNormal" style="margin:0cm 0cm 0.0001pt;text-align:center;line-height:normal;font-size:11pt;font-family:&quot;Calibri&quot;,sans-serif" align="center"><span style="color:black"><span>&nbsp;</span>{}<span></span></span></p>\n'.format(cell)
+                        html += '\t\t</td>\n'
+
+                html += '\t</tr>\n'
+        html += '</tbody></table>\n'
+        return html
