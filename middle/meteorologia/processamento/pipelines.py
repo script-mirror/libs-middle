e6b00e542a7a324c4b8bbe2aac52261bd8bf9ccc
josepaulo1233 <josehpaulo2124@gmail.com>
2025-10-02 14:12:35 -0300

    feat: add function to generate total accumulation in pipelines for 'sfc' type

--- middle/meteorologia/processamento/pipelines.py
+++ middle/meteorologia/processamento/pipelines.py
@@ -60,6 +60,7 @@ def pipelines(modelo, produtos, tipo=None, hora=None):
                 lambda: produtos.gerar_semanas_operativas(extent=CONSTANTES['extents_mapa']['brasil'], add_valor_bacias=True),
                 lambda: produtos.gerar_media_bacia_smap(plot_graf=True, ensemble=True, salva_db=False),
                 lambda: produtos.gerar_prec24h(extent=CONSTANTES['extents_mapa']['brasil']),
+                lambda: produtos.gerar_acumulado_total(extent=CONSTANTES['extents_mapa']['brasil'], add_valor_bacias=True),
             ]
 
         elif tipo == 'pl':
