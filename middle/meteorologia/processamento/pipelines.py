227758407470a6ae2e412c8493afdf988d211fd4
josepaulo1233 <josehpaulo2124@gmail.com>
2025-10-09 14:35:22 -0300

    fix: uncomment 'gerar_estacao_chuvosa' function calls for 'sudeste' region in pipelines function

--- middle/meteorologia/processamento/pipelines.py
+++ middle/meteorologia/processamento/pipelines.py
@@ -129,7 +129,7 @@ def pipelines(modelo, produtos, tipo=None, hora=None):
                 lambda: produtos.gerar_media_bacia_smap(plot_graf=True, ensemble=True, salva_db=False),
                 lambda: produtos.gerar_prec24h(extent=CONSTANTES['extents_mapa']['brasil']),
                 lambda: produtos.gerar_acumulado_total(extent=CONSTANTES['extents_mapa']['brasil'], add_valor_bacias=True),
-                # lambda: produtos.gerar_estacao_chuvosa(regiao_estacao_chuvosa='sudeste'),
+                lambda: produtos.gerar_estacao_chuvosa(regiao_estacao_chuvosa='sudeste'),
                 # lambda: produtos.gerar_estacao_chuvosa(regiao_estacao_chuvosa='norte'),     
             ]
 
@@ -146,7 +146,7 @@ def pipelines(modelo, produtos, tipo=None, hora=None):
                 lambda: produtos.gerar_media_bacia_smap(plot_graf=True, ensemble=True, salva_db=False),
                 lambda: produtos.gerar_prec24h(extent=CONSTANTES['extents_mapa']['brasil']),
                 lambda: produtos.gerar_acumulado_total(extent=CONSTANTES['extents_mapa']['brasil'], add_valor_bacias=True),
-                # lambda: produtos.gerar_estacao_chuvosa(regiao_estacao_chuvosa='sudeste'),
+                lambda: produtos.gerar_estacao_chuvosa(regiao_estacao_chuvosa='sudeste'),
                 # lambda: produtos.gerar_estacao_chuvosa(regiao_estacao_chuvosa='norte'),     
             ]
 
