b512d64358d7340d233b8b483754b2d82582ee98
josepaulo1233 <josehpaulo2124@gmail.com>
2025-10-30 14:33:21 -0300

    fix: update time coordinate assignment in ConfigProdutosObservado to use the last valid_time

--- middle/meteorologia/processamento/produtos.py
+++ middle/meteorologia/processamento/produtos.py
@@ -1056,7 +1056,7 @@ class ConfigProdutosObservado:
 
             elif modelo_fmt in ['cpc']:
                 ds = ds.rename({'time': 'valid_time'})
-                ds = ds.assign_coords(time=ds.valid_time)
+                ds = ds.assign_coords(time=ds.valid_time[-1])  # Adiciona a coordenada 'time' como o último valid_time
 
         if todo_dir:
             files = [f'{caminho_para_salvar}/{f}' for f in files if f.endswith((".grib2", ".grb", ".nc"))]
